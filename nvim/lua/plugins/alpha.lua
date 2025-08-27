ff9009531b0f86012e7fa96541dfdce8d9fdc1db
Abuzar <theopenorbit@gmail.com>
2025-08-27 19:08:18 +0500

    nvim: refactor usage of auto-session for new API

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,37 +1,37 @@
 return {
-  'goolord/alpha-nvim',
-  config = function ()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-    }
+		-- Set header
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-    -- Menu
-    dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
-      dashboard.button("n", "  New file", "<cmd>ene <BAR> startinsert<CR>"),
-      dashboard.button("g", "  Live grep", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("e", "  File explorer", "<cmd>Neotree toggle<CR>"),
-      dashboard.button("s", "  Restore last session", "<cmd>SessionRestore<CR>"),
-      dashboard.button("S", "  Search sessions", "<cmd>SessionSearch<CR>"),
-      dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
-    }
+		-- Menu
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("n", "  New file", "<cmd>ene <BAR> startinsert<CR>"),
+			dashboard.button("g", "  Live grep", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("e", "  File explorer", "<cmd>Neotree toggle<CR>"),
+			dashboard.button("s", "  Restore last session", "<cmd>AutoSession restore<CR>"),
+			dashboard.button("S", "  Search sessions", "<cmd>AutoSession search<CR>"),
+			dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+		}
 
-    -- Footer
-    dashboard.section.footer.val = "⚡ Neovim loaded successfully!"
+		-- Footer
+		dashboard.section.footer.val = "⚡ Neovim loaded successfully!"
 
-    -- Final setup
-    alpha.setup(dashboard.config)
-  end
-};
+		-- Final setup
+		alpha.setup(dashboard.config)
+	end,
+}
--- nvim/lua/plugins/auto-sessions.lua
+++ nvim/lua/plugins/auto-sessions.lua
@@ -1,15 +1,15 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
-  keys = {
-    -- Will use Telescope if installed or a vim.ui.select picker otherwise
-    { "<leader>ws", "<cmd>SessionSearch<CR>", desc = "Session search" },
-    { "<leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session" },
-    { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
-  },
+	"rmagatti/auto-session",
+	lazy = false,
+	keys = {
+		-- Will use Telescope if installed or a vim.ui.select picker otherwise
+		{ "<leader>ws", "<cmd>AutoSession search<CR>", desc = "Session search" },
+		{ "<leader>wr", "<cmd>AutoSession restore<CR>", desc = "Restore session" },
+		{ "<leader>wa", "<cmd>AutoSession toggle<CR>", desc = "Toggle autosave" },
+	},
 
-  opts = {
-    auto_restore = false,
-    suppressed_dirs = { "~/", "~/Downloads", "/" },
-  },
+	opts = {
+		auto_restore = false,
+		suppressed_dirs = { "~/", "~/Downloads", "/" },
+	},
 }
